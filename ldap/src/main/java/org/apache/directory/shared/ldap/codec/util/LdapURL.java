ad6071e185ce24589cd41212510dbb73f0fd2fee
Emmanuel Lecharny <elecharny@apache.org>
2007-01-18 16:09:49 +0000

    The UrlEncode method is now static and public
    git-svn-id: https://svn.apache.org/repos/asf/directory/branches/shared/0.9.5@497479 13f79535-47bb-0310-9956-ffa450edef68

--- ldap/src/main/java/org/apache/directory/shared/ldap/codec/util/LdapURL.java
+++ ldap/src/main/java/org/apache/directory/shared/ldap/codec/util/LdapURL.java
@@ -1153,7 +1153,7 @@ public class LdapURL
      * @param doubleEncode Set if we need to encode the comma
      * @return An encoded string
      */
-    private String urlEncode( String string, boolean doubleEncode )
+    public static String urlEncode( String string, boolean doubleEncode )
     {
         StringBuffer sb = new StringBuffer();
 
