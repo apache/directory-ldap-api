fafeeb8d9aca91ba7ebdfa2d0f06dc612e8284ea
Emmanuel Lecharny <elecharny@apache.org>
2007-03-05 18:34:09 +0000

    replaced the getType() method by a call to the getNormType() or getUpType() methods.
    git-svn-id: https://svn.apache.org/repos/asf/directory/shared/branches/0.9.5@514789 13f79535-47bb-0310-9956-ffa450edef68

--- ldap/src/main/java/org/apache/directory/shared/ldap/name/LdapDN.java
+++ ldap/src/main/java/org/apache/directory/shared/ldap/name/LdapDN.java
@@ -1261,14 +1261,14 @@ public class LdapDN /* extends LdapString */implements Name
    private static AttributeTypeAndValue atavOidToName( AttributeTypeAndValue atav, Map oidsMap )
        throws InvalidNameException, NamingException
    {
-       String type = StringTools.trim( atav.getType() );
+       String type = StringTools.trim( atav.getNormType() );
 
        if ( ( type.startsWith( "oid." ) ) || ( type.startsWith( "OID." ) ) )
        {
            type = type.substring( 4 );
        }
 
-       if ( StringTools.isNotEmpty( StringTools.lowerCase( type ) ) )
+       if ( StringTools.isNotEmpty( type ) )
        {
            if ( oidsMap == null )
            {
@@ -1328,20 +1328,20 @@ public class LdapDN /* extends LdapString */implements Name
            {
                Object val = atavs.next();
                AttributeTypeAndValue newAtav = atavOidToName( ( AttributeTypeAndValue ) val, oidsMap );
-               rdn.addAttributeTypeAndValue( newAtav.getType(), newAtav.getValue() );
+               rdn.addAttributeTypeAndValue( newAtav.getUpType(), newAtav.getValue() );
            }
 
        }
        else
        {
-           String type = StringTools.trim( rdn.getType() );
+           String type = rdn.getNormType();
 
            if ( ( type.startsWith( "oid." ) ) || ( type.startsWith( "OID." ) ) )
            {
                type = type.substring( 4 );
            }
 
-           if ( StringTools.isNotEmpty( StringTools.lowerCase( type ) ) )
+           if ( StringTools.isNotEmpty( type ) )
            {
                if ( oidsMap == null )
                {
@@ -1357,11 +1357,11 @@ public class LdapDN /* extends LdapString */implements Name
                        Rdn rdnCopy = ( Rdn ) rdn.clone();
                        rdn.clear();
                        Object value = rdnCopy.getValue();
-                       value = DefaultStringNormalizer.normalizeString( ( String ) value );
+                       Object normValue = DefaultStringNormalizer.normalizeString( ( String ) value );
 
                        rdn.addAttributeTypeAndValue( oidNormalizer.getAttributeTypeOid(),
                            ( String ) oidNormalizer.getNormalizer()
-                           .normalize( value ) );
+                           .normalize( normValue ) );
 
                    }
                    else
