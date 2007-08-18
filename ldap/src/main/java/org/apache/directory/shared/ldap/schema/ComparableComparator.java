1be6a89ab73f30d1645c6ba43501da68d68fd70a
Alex Karasulu <akarasulu@apache.org>
2007-08-18 00:04:14 +0000

    darn it - merge introduced jdk 5 annotation which breaks build: annotation removed
    git-svn-id: https://svn.apache.org/repos/asf/directory/shared/branches/0.9.5@567185 13f79535-47bb-0310-9956-ffa450edef68

--- ldap/src/main/java/org/apache/directory/shared/ldap/schema/ComparableComparator.java
+++ ldap/src/main/java/org/apache/directory/shared/ldap/schema/ComparableComparator.java
@@ -30,7 +30,6 @@ import java.io.Serializable;
  * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
  * @version $Rev$
  */
-@SuppressWarnings("unchecked")
 public class ComparableComparator implements Comparator, Serializable
 {
     static final long serialVersionUID = -5295278271807198471L;
