650804f82741f2edfa8670bba5f94b48a532d18c
Emmanuel Lecharny <elecharny@apache.org>
2014-04-16 16:47:19 +0000

    Added the implementatio of the AdDirSyncResponse codec
    git-svn-id: https://svn.apache.org/repos/asf/directory/shared/trunk@1587971 13f79535-47bb-0310-9956-ffa450edef68

--- ldap/extras/codec-api/src/main/java/org/apache/directory/api/ldap/extras/controls/ad/AdDirSyncResponse.java
+++ ldap/extras/codec-api/src/main/java/org/apache/directory/api/ldap/extras/controls/ad/AdDirSyncResponse.java
@@ -22,10 +22,67 @@ package org.apache.directory.api.ldap.extras.controls.ad;
 import org.apache.directory.api.ldap.model.message.Control;
 
 /**
+ * The AdDirSyncResponse control has the following value :
  * 
+ * <pre>
+ * Repl    Control ::= SEQUENCE {
+ *     controlType             1.2.840.113556.1.4.841
+ *     controlValue            replControlValue
+ *     criticality             TRUE
+ * }
+ * 
+ * realReplControlValue ::= SEQUENCE {
+ *     flag                    integer
+ *     maxReturnLength       integer
+ *     cookie                  OCTET STRING
+ * }
+ * </pre>
  * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
  */
 public interface AdDirSyncResponse extends Control
 {
-
+    /** This control OID */
+    static final String OID = "1.2.840.113556.1.4.841";
+    
+    /**
+     * @return The flag returned by the server. One of :
+     * <ul>
+     * <li>LDAP_DIRSYNC_OBJECT_SECURITY (0x0001)</li>
+     * <li>LDAP_DIRSYNC_ANCESTORS_FIRST_ORDER (0x0800)</li>
+     * <li>LDAP_DIRSYNC_PUBLIC_DATA_ONLY (0x2000)(</li>
+     * <li>LDAP_DIRSYNC_INCREMENTAL_VALUES (0x7FFFFFFF)</li>
+     * </ul>
+     */
+    AdDirSyncFlag getFlag();
+    
+    
+    /**
+     * @param flag The flag. 
+     */
+    void setFlag( AdDirSyncFlag flag );
+    
+    
+    /**
+     * @return The maximum length of attributes to be returned
+     */
+    int getMaxReturnLength();
+    
+    
+    /**
+     * @param maxReturnLength The maximum length of attributes to be returned
+     */
+    void setMaxReturnLength( int maxReturnLength );
+    
+    
+    /**
+     * @return The cookie used while processing the successive DirSync operations
+     */
+    byte[] getCookie();
+    
+    
+    /**
+     * @param cookie The cookie to send to the server. It's the value found in the response control. Should be null
+     * for the first control.
+     */
+    void setCookie( byte[] cookie );
 }
--- ldap/extras/codec-api/src/main/java/org/apache/directory/api/ldap/extras/controls/ad/AdDirSyncResponseImpl.java
+++ ldap/extras/codec-api/src/main/java/org/apache/directory/api/ldap/extras/controls/ad/AdDirSyncResponseImpl.java
@@ -19,11 +19,92 @@
  */
 package org.apache.directory.api.ldap.extras.controls.ad;
 
+import org.apache.directory.api.ldap.model.message.controls.AbstractControl;
+import org.apache.directory.api.util.Strings;
+
 /**
+ * The class implementing the AdDirSyncResponse
  * 
  * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
  */
-public class AdDirSyncResponseImpl
+public class AdDirSyncResponseImpl extends AbstractControl implements AdDirSyncResponse
 {
+    /** A flag used to indicate that there are more data to return */
+    AdDirSyncFlag flag = AdDirSyncFlag.DEFAULT;
+    
+    /** The maximum number of attributes to return */
+    int maxReturnLength = 0;
+    
+    /** The DirSync cookie */
+    private byte[] cookie;
 
+    /**
+     * Default constructor for this control
+     */
+    public AdDirSyncResponseImpl()
+    {
+        super( OID, Boolean.TRUE );
+    }
+    
+
+    /**
+     * {@inheritDoc}
+     */
+    public AdDirSyncFlag getFlag()
+    {
+        return flag;
+    }
+    
+
+    /**
+     * {@inheritDoc}
+     */
+    public void setFlag( AdDirSyncFlag flag )
+    {
+        this.flag = flag;
+    }
+    
+
+    /**
+     * {@inheritDoc}
+     */
+    public int getMaxReturnLength()
+    {
+        return maxReturnLength;
+    }
+
+
+    /**
+     * {@inheritDoc}
+     */
+    public void setMaxReturnLength( int maxReturnLength )
+    {
+        this.maxReturnLength = maxReturnLength;
+    }
+
+
+    /**
+     * {@inheritDoc}
+     */
+    public byte[] getCookie()
+    {
+        return cookie;
+    }
+
+
+    /**
+     * {@inheritDoc}
+     */
+    public void setCookie( byte[] cookie )
+    {
+        if ( cookie != null )
+        {
+            this.cookie = new byte[cookie.length];
+            System.arraycopy( cookie, 0, this.cookie, 0,cookie.length );
+        }
+        else
+        {
+            this.cookie = Strings.EMPTY_BYTES;
+        }
+    }
 }
