31c3688183d0865dcaebefe4e547424d5918aefd
Lucas Theisen <lucastheisen@apache.org>
2014-04-26 22:58:39 +0000

    Added mandatory Apache License 2.0 comment and class javadoc
    git-svn-id: https://svn.apache.org/repos/asf/directory/shared/trunk@1590323 13f79535-47bb-0310-9956-ffa450edef68

--- ldap/client/api/src/main/java/org/apache/directory/ldap/client/api/DefaultLdapConnectionFactory.java
+++ ldap/client/api/src/main/java/org/apache/directory/ldap/client/api/DefaultLdapConnectionFactory.java
@@ -1,3 +1,22 @@
+/*
+ *   Licensed to the Apache Software Foundation (ASF) under one
+ *   or more contributor license agreements.  See the NOTICE file
+ *   distributed with this work for additional information
+ *   regarding copyright ownership.  The ASF licenses this file
+ *   to you under the Apache License, Version 2.0 (the
+ *   "License"); you may not use this file except in compliance
+ *   with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing,
+ *   software distributed under the License is distributed on an
+ *   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *   KIND, either express or implied.  See the License for the
+ *   specific language governing permissions and limitations
+ *   under the License.
+ *
+ */
 package org.apache.directory.ldap.client.api;
 
 
@@ -9,6 +28,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
+/**
+ * The default implementation of LdapConnectionFactory. Allows for the 
+ * setting of timeout and {@link LdapApiService} as well as the standard 
+ * {@link LdapConnectionConfig}.
+ * 
+ * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
+ */
 public class DefaultLdapConnectionFactory implements LdapConnectionFactory
 {
     private static Logger LOG = LoggerFactory.getLogger( DefaultLdapConnectionFactory.class );
@@ -18,6 +44,11 @@ public class DefaultLdapConnectionFactory implements LdapConnectionFactory
     private long timeout;
 
 
+    /**
+     * Creates a new instance of DefaultLdapConnectionFactory.
+     *
+     * @param config The connection config.
+     */
     public DefaultLdapConnectionFactory( LdapConnectionConfig config )
     {
         this.connectionConfig = config;
