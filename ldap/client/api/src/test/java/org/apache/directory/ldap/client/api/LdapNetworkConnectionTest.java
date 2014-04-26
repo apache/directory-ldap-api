af52c2b458d1a303671dcadf8686d897b6c28736
Stefan Seelmann <seelmann@apache.org>
2014-04-26 23:04:58 +0000

    Forgot license
    git-svn-id: https://svn.apache.org/repos/asf/directory/shared/trunk@1590324 13f79535-47bb-0310-9956-ffa450edef68

--- ldap/client/api/src/test/java/org/apache/directory/ldap/client/api/LdapNetworkConnectionTest.java
+++ ldap/client/api/src/test/java/org/apache/directory/ldap/client/api/LdapNetworkConnectionTest.java
@@ -1,3 +1,22 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *  
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License. 
+ *  
+ */
 package org.apache.directory.ldap.client.api;
 
 
@@ -12,6 +31,11 @@ import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
 
+/**
+ * Tests LdapNetworkConnection.
+ * 
+ * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
+ */
 @RunWith(Parameterized.class)
 public class LdapNetworkConnectionTest
 {
@@ -43,8 +67,7 @@ public class LdapNetworkConnectionTest
                     { Long.MAX_VALUE, -1, Long.MAX_VALUE, "Invalid search time limit, use connection timeout" },
                     { Long.MAX_VALUE, 0, Long.MAX_VALUE, "Search time limit is 0, use max value" },
                     { Long.MAX_VALUE, 1, Long.MAX_VALUE,
-                        "search time limit < connection timeout, use connection timeout" },
-            } );
+                        "search time limit < connection timeout, use connection timeout" }, } );
     }
 
 
