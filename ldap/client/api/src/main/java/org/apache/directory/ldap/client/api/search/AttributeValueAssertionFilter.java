b94418e722b4826a7d9872e3ed4c84eccf806030
Emmanuel Lecharny <elecharny@apache.org>
2015-01-26 18:58:09 +0000

    Added the missing AL2.0 license, and the class header placeholder
    git-svn-id: https://svn.apache.org/repos/asf/directory/shared/trunk@1654868 13f79535-47bb-0310-9956-ffa450edef68

--- ldap/client/api/src/main/java/org/apache/directory/ldap/client/api/search/AttributeValueAssertionFilter.java
+++ ldap/client/api/src/main/java/org/apache/directory/ldap/client/api/search/AttributeValueAssertionFilter.java
@@ -1,54 +1,77 @@
-
+/*
+ *   Licensed to the Apache Software Foundation (ASF) under one
+ *   or more contributor license agreements.  See the NOTICE file
+ *   distributed with this work for additional information
+ *   regarding copyright ownership.  The ASF licenses this file
+ *   to you under the Apache License, Version 2.0 (the
+ *   "License"); you may not use this file except in compliance
+ *   with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing,
+ *   software distributed under the License is distributed on an
+ *   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *   KIND, either express or implied.  See the License for the
+ *   specific language governing permissions and limitations
+ *   under the License.
+ *
+ */
 package org.apache.directory.ldap.client.api.search;
 
 
+/**
+ * 
+ * TODO AttributeValueAssertionFilter.
+ *
+ * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
+ */
 class AttributeValueAssertionFilter extends AbstractFilter
 {
     private String attribute;
     private String value;
     private Operator operator;
-    
-    
+
+
     private AttributeValueAssertionFilter( String attribute, String value, Operator operator )
     {
         this.attribute = attribute;
         this.value = value;
         this.operator = operator;
     }
-    
-    
-    public static AttributeValueAssertionFilter approximatelyEqual( String attribute, String value ) 
+
+
+    public static AttributeValueAssertionFilter approximatelyEqual( String attribute, String value )
     {
         return new AttributeValueAssertionFilter( attribute, value, Operator.APPROXIMATELY_EQUAL );
     }
-    
-    
-    public static AttributeValueAssertionFilter equal( String attribute, String value ) 
+
+
+    public static AttributeValueAssertionFilter equal( String attribute, String value )
     {
         return new AttributeValueAssertionFilter( attribute, value, Operator.EQUAL );
     }
-    
-    
-    public static AttributeValueAssertionFilter greaterThanOrEqual( String attribute, String value ) 
+
+
+    public static AttributeValueAssertionFilter greaterThanOrEqual( String attribute, String value )
     {
         return new AttributeValueAssertionFilter( attribute, value, Operator.GREATER_THAN_OR_EQUAL );
     }
-    
-    
-    public static AttributeValueAssertionFilter lessThanOrEqual( String attribute, String value ) 
+
+
+    public static AttributeValueAssertionFilter lessThanOrEqual( String attribute, String value )
     {
         return new AttributeValueAssertionFilter( attribute, value, Operator.LESS_THAN_OR_EQUAL );
     }
-    
+
 
     @Override
     public StringBuilder build( StringBuilder builder )
     {
         return builder.append( "(" ).append( attribute )
-                .append( operator.operator() )
-                .append( value ).append( ")" );
+            .append( operator.operator() )
+            .append( value ).append( ")" );
     }
-
 
     public static enum Operator
     {
@@ -56,15 +79,17 @@ class AttributeValueAssertionFilter extends AbstractFilter
         EQUAL("="),
         GREATER_THAN_OR_EQUAL(">="),
         LESS_THAN_OR_EQUAL("<=");
-        
+
         private String operator;
-        
-        private Operator( String operator ) 
+
+
+        private Operator( String operator )
         {
             this.operator = operator;
         }
-        
-        public String operator() 
+
+
+        public String operator()
         {
             return operator;
         }
--- ldap/client/api/src/main/java/org/apache/directory/ldap/client/api/search/FilterBuilder.java
+++ ldap/client/api/src/main/java/org/apache/directory/ldap/client/api/search/FilterBuilder.java
@@ -1,3 +1,22 @@
+/*
+ *   Licensed to the Apache Software Foundation (ASF) under one
+ *   or more contributor license agreements.  See the NOTICE file
+ *   distributed with this work for additional information
+ *   regarding copyright ownership.  The ASF licenses this file
+ *   to you under the Apache License, Version 2.0 (the
+ *   "License"); you may not use this file except in compliance
+ *   with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing,
+ *   software distributed under the License is distributed on an
+ *   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *   KIND, either express or implied.  See the License for the
+ *   specific language governing permissions and limitations
+ *   under the License.
+ *
+ */
 package org.apache.directory.ldap.client.api.search;
 
 
@@ -57,10 +76,12 @@ public class FilterBuilder
     public static FilterBuilder and( FilterBuilder... filters )
     {
         SetOfFiltersFilter filter = SetOfFiltersFilter.and();
+
         for ( FilterBuilder builder : filters )
         {
             filter.add( builder.filter() );
         }
+
         return new FilterBuilder( filter );
     }
 
@@ -215,10 +236,12 @@ public class FilterBuilder
     public static FilterBuilder or( FilterBuilder... filters )
     {
         SetOfFiltersFilter filter = SetOfFiltersFilter.or();
+
         for ( FilterBuilder builder : filters )
         {
             filter.add( builder.filter() );
         }
+
         return new FilterBuilder( filter );
     }
 
