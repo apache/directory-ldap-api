a8d397add063db325ba7f9ae0d7e7f014bd6f0fb
Emmanuel Lecharny <elecharny@apache.org>
2015-09-10 14:09:07 +0000

    Improved the Javadoc : LDAPUrlValue for scope does not make a lot of sense.
    git-svn-id: https://svn.apache.org/repos/asf/directory/shared/trunk@1702257 13f79535-47bb-0310-9956-ffa450edef68

--- ldap/model/src/main/java/org/apache/directory/api/ldap/model/message/SearchScope.java
+++ ldap/model/src/main/java/org/apache/directory/api/ldap/model/message/SearchScope.java
@@ -115,28 +115,28 @@ public enum SearchScope
 
     /**
      * Gets the SearchScope enumerated type for the corresponding 
-     * LDAP URL scope value of either base, one or sub.
+     * scope value of either base, one or sub.
      *
-     * @param ldapUrlValue the LDAP URL scope value to get SearchScope for
+     * @param scope the scope value to get SearchScope for
      * @return the SearchScope enumerated type for the LDAP URL scope value
      */
-    public static int getSearchScope( String ldapUrlValue )
+    public static int getSearchScope( String scope )
     {
-        if ( "base".equalsIgnoreCase( ldapUrlValue ) )
+        if ( "base".equalsIgnoreCase( scope ) )
         {
             return OBJECT.getScope();
         }
-        else if ( "one".equalsIgnoreCase( ldapUrlValue ) )
+        else if ( "one".equalsIgnoreCase( scope ) )
         {
             return ONELEVEL.getScope();
         }
-        else if ( "sub".equalsIgnoreCase( ldapUrlValue ) )
+        else if ( "sub".equalsIgnoreCase( scope ) )
         {
             return SUBTREE.getScope();
         }
         else
         {
-            throw new IllegalArgumentException( I18n.err( I18n.ERR_04161, ldapUrlValue ) );
+            throw new IllegalArgumentException( I18n.err( I18n.ERR_04161, scope ) );
         }
     }
 
