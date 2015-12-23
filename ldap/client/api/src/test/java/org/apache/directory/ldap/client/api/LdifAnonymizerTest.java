b61c9cbd5791423e08659fbaf1ee4bce16834c4d
Emmanuel Lécharny <elecharny@apache.org>
2015-12-23 14:40:11 +0000

    Added some tests for the anonymization of change operations
    git-svn-id: https://svn.apache.org/repos/asf/directory/shared/trunk@1721546 13f79535-47bb-0310-9956-ffa450edef68

--- ldap/client/api/src/test/java/org/apache/directory/ldap/client/api/LdifAnonymizerTest.java
+++ ldap/client/api/src/test/java/org/apache/directory/ldap/client/api/LdifAnonymizerTest.java
@@ -21,10 +21,26 @@
 package org.apache.directory.ldap.client.api;
 
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.directory.api.ldap.model.entry.Attribute;
+import org.apache.directory.api.ldap.model.entry.Modification;
+import org.apache.directory.api.ldap.model.entry.ModificationOperation;
+import org.apache.directory.api.ldap.model.entry.Value;
+import org.apache.directory.api.ldap.model.ldif.LdifEntry;
+import org.apache.directory.api.ldap.model.ldif.LdifReader;
 import org.apache.directory.api.ldap.model.schema.SchemaManager;
 import org.apache.directory.api.ldap.schema.manager.impl.DefaultSchemaManager;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotSame;
 
 
 /**
@@ -36,6 +52,9 @@ public class LdifAnonymizerTest
 {
     private static SchemaManager schemaManager;
     
+    private static LdifReader ldifReader;
+
+    
     @BeforeClass
     public static void setup()
     {
@@ -44,6 +63,7 @@ public class LdifAnonymizerTest
         try
         {
             schemaManager = new DefaultSchemaManager();
+            ldifReader = new LdifReader( schemaManager );
         }
         catch ( Exception e )
         {
@@ -55,7 +75,7 @@ public class LdifAnonymizerTest
     
     
     @Test
-    public void testLdifAnonymizer() throws Exception, Exception
+    public void testLdifAnonymizer() throws Exception
     {
         String ldif =
             "dn: cn=test,dc=example,dc=com\n" +
@@ -100,7 +120,7 @@ public class LdifAnonymizerTest
 
 
     @Test
-    public void testLdifAnonymizer2() throws Exception, Exception
+    public void testLdifAnonymizer2() throws Exception
     {
         String ldif =
             "dn: cn=cn2 + sn=elecharny, dc=example, dc=com\n" +
@@ -119,7 +139,7 @@ public class LdifAnonymizerTest
 
 
     @Test
-    public void testLdifAnonymizer3() throws Exception, Exception
+    public void testLdifAnonymizer3() throws Exception
     {
         String ldif =
             "dn: cn=cn2 + sn=elecharny, dc=example, dc=com\n" +
@@ -139,67 +159,87 @@ public class LdifAnonymizerTest
 
 
     @Test
-    public void testLdifAnonymizer4() throws Exception, Exception
+    public void testLdifAnonymizerChangeType() throws Exception
     {
         String ldif =
-            "dn: ou=PD Accountants, ou=Product Development, ou=usa, dc=airius, dc=com\n" +
-            "changetype: modrdn\n" +
-            "newrdn: ou=Product Development Accountants\n" +
-            "deleteoldrdn: 0\n" +
-            "newsuperior: ou=Accounting, ou=usa, dc=airius, dc=com\n" +
-            "\n" +
-            "dn: cn=Paula Jensen, ou=Product Development, dc=airius, dc=com\n" +
-            "changetype: modify\n" +
-            "add: postaladdress\n" +
-            "postaladdress: 123 Anystreet $ Sunnyvale, CA $ 94086\n" +
-            "-\n" +
-            "delete: description\n" +
-            "-\n" +
-            "replace: telephonenumber\n" +
-            "telephonenumber: +1 408 555 1234\n" +
-            "telephonenumber: +1 408 555 5678\n" +
-            "-\n" +
-            "delete: facsimiletelephonenumber\n" +
-            "facsimiletelephonenumber: +1 408 555 9876\n" +
-            "-\n" +
-            "\n" +
-            "dn: cn=cn2,ou=People,o=hp.com\n" +
-            "changetype: add\n" +
-            "ObjectClass: top\n" +
-            "objectClass: person\n" +
-            "cn: cn1\n" +
-            "cn: cn2\n" +
-            "cn: cn3\n" +
-            "userPassword: test\n" +
-            "sn: elecharny\n" +
-            "givenname: test\n" +
-            "\n" +
-            "dn: uid=thayapari.a.wijesundara@hp.com,ou=People,o=hp.com\n" +
-            "changetype: delete\n" +
-            "\n" +
-            "dn: uid=thayapari.a.wijesundara@hp.com,ou=People,o=hp.com\n" +
-            "changetype: modify\n" +
-            "replace: telephoneNumber\n" +
-            "telephoneNumber::KzYxIDIgODI3ODQ2NDQ=\n" +
-            "-\n" +
-            "\n" +
-            "dn: uid=thayapari.a.wijesundara@hp.com,ou=People,o=hp.com\n" +
-            "changetype: modify\n" +
-            "replace: telephoneNumber\n" +
-            "telephoneNumber::KzYxIDIgODI3ODQ2NDQ=\n" +
-            "-\n" +
-            "\n" +
-            "dn: cn=vsmuser_g1u2283c,ou=Groups,o=hp.com\n" +
+            "dn: cn=test,ou=Groups,o=acme,dc=com\n" +
             "changetype: modify\n" +
             "replace: member\n" +
-            "member::Y249dnNtLmhvdXN0b24uaHAuY29tLG91PVNlcnZlcnMsbz1ocC5jb20=\n" +
-            "member::dWlkPXRpbS50dXNzaW5nQGhwLmNvbSxvdT1QZW9wbGUsbz1ocC5jb20=\n" +
-            "member::dWlkPXRpbS50dXNzaW5nQGhwZS5jb20sb3U9UGVvcGxlLG89aHAuY29t\n" +
-            "member::dWlkPW1hcnRoYWxhLm5pci5yZWRkeUBocGUuY29tLG91PVBlb3BsZSxvPWhwLmNvbQ==\n" +
+            "member::Y249YWNtZTEuY29tLG91PVNlcnZlcnMsbz1hY21lLGRjPWNvbQ==\n" +                  // cn=acme1.com,ou=Servers,o=acme,dc=com
+            "member::dWlkPWpvaG4uZG9lQGFjbWUuY29tLG91PVBlb3BsZSxvPWFjbWUsZGM9Y29t\n" +          // uid=john.doe@acme.com,ou=People,o=acme,dc=com
+            "member::dWlkPWphY2suZG9lQGFjbWUuY29tLG91PVBlb3BsZSxvPWFjbWUsZGM9Y29t\n" +          // uid=jack.doe@acme.com,ou=People,o=acme,dc=com
+            "member::dWlkPWppbS5nb256YWxlc0BhY21lLmNvbSxvdT1QZW9wbGUsbz1hY21lLGRjPWNvbQ==\n" +  // uid=jim.gonzales@acme.com,ou=People,o=acme,dc=com
             "-";
         
         LdifAnonymizer anonymizer = new LdifAnonymizer( schemaManager );
-        anonymizer.addNamingContext( "dc=example,dc=com" );
-        anonymizer.anonymize( ldif );
+        anonymizer.addNamingContext( "o=acme,dc=com" );
+        String result = anonymizer.anonymize( ldif );
+        
+        List<LdifEntry> entries = ldifReader.parseLdif( result );
+        
+        assertEquals( 1, entries.size() );
+        
+        LdifEntry entry = entries.get( 0 );
+        assertTrue( entry.isChangeModify() );
+        assertEquals( 1, entry.getModifications().size() );
+        
+        Modification modification = entry.getModifications().get( 0 );
+        assertEquals( ModificationOperation.REPLACE_ATTRIBUTE, modification.getOperation() );
+
+        Attribute attribute = modification.getAttribute();
+        assertEquals( "member", attribute.getUpId() );
+        assertEquals( 4, attribute.size() );
+        
+        Set<String> originalValues = new HashSet<String>();
+        originalValues.add( "cn=acme1.com,ou=Servers,o=acme,dc=com" );
+        originalValues.add( "uid=john.doe@acme.com,ou=People,o=acme,dc=com" );
+        originalValues.add( "uid=jack.doe@acme.com,ou=People,o=acme,dc=com" );
+        originalValues.add( "uid=jim.gonzales@acme.com,ou=People,o=acme,dc=com" );
+        
+        for ( Value<?> value : attribute )
+        {
+            String str = value.getString();
+            
+            // We can only test the length and teh fact teh values are not equal (as the vale has been anonymized)
+            assertNotSame( 0, value.length() );
+            assertFalse( originalValues.contains( str ) );
+            assertTrue( str.endsWith( ",o=acme,dc=com" ) );
+        }
+    }
+    
+    
+    @Test
+    public void testAnonymizeModify() throws Exception
+    {
+        String ldif = 
+            "dn: mail=legal@acme.com,ou=Email,ou=Services,o=acme,dc=com\n" +
+            "changetype: modify\n" +
+            "replace: cn\n" +
+            "cn::QUNNRSBJbmMuIExlZ2FsIFRlYW0=\n" +
+            "-";
+        LdifAnonymizer anonymizer = new LdifAnonymizer( schemaManager );
+        anonymizer.addNamingContext( "o=acm,dc=com" );
+        String result = anonymizer.anonymize( ldif );
+        
+        List<LdifEntry> entries = ldifReader.parseLdif( result );
+        
+        assertEquals( 1, entries.size() );
+        
+        LdifEntry entry = entries.get( 0 );
+        assertTrue( entry.isChangeModify() );
+        assertEquals( 1, entry.getModifications().size() );
+        
+        Modification modification = entry.getModifications().get( 0 );
+        assertEquals( ModificationOperation.REPLACE_ATTRIBUTE, modification.getOperation() );
+
+        Attribute attribute = modification.getAttribute();
+        assertEquals( "cn", attribute.getUpId() );
+        assertEquals( 1, attribute.size() );
+        
+        String value = attribute.getString();
+        
+        // We can only test the length and teh fact teh values are not equal (as the vale has been anonymized)
+        assertEquals( "ACME Inc. Legal Team".length(), value.length() );
+        assertNotEquals( "ACME Inc. Legal Team", value );
     }
 }
