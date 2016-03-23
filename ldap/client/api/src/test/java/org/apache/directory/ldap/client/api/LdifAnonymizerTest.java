383b2d84d96132c19532dc85b1ca58ff22a4182a
Emmanuel Lécharny <elecharny@apache.org>
2016-03-23 18:36:26 +0000

    Removed useless imports
    git-svn-id: https://svn.apache.org/repos/asf/directory/shared/trunk@1736369 13f79535-47bb-0310-9956-ffa450edef68

--- ldap/client/api/src/test/java/org/apache/directory/ldap/client/api/LdifAnonymizerTest.java
+++ ldap/client/api/src/test/java/org/apache/directory/ldap/client/api/LdifAnonymizerTest.java
@@ -41,9 +41,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotSame;
 
 
