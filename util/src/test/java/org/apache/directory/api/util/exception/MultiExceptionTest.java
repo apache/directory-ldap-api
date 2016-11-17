b2463ced3bd25383e46ff3e1c5a9d10a6f332fd6
Emmanuel Lécharny <elecharny@apache.org>
2016-11-17 23:53:43 +0000

    Added the missing headers
    git-svn-id: https://svn.apache.org/repos/asf/directory/shared/trunk@1770288 13f79535-47bb-0310-9956-ffa450edef68

--- util/src/test/java/org/apache/directory/api/util/exception/MultiExceptionTest.java
+++ util/src/test/java/org/apache/directory/api/util/exception/MultiExceptionTest.java
@@ -1,6 +1,23 @@
 package org.apache.directory.api.util.exception;
-
-
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *  
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License. 
+ *  
+ */
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
@@ -15,9 +32,12 @@ import org.junit.Before;
 import org.junit.Test;
 
 
+/**
+ * 
+ * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
+ */
 public class MultiExceptionTest
 {
-
     private PrintStream originalOut;
     private PrintStream originalErr;
 
@@ -80,5 +100,4 @@ public class MultiExceptionTest
         assertThat( customOut.toString(), containsString( "nested2" ) );
         assertThat( systemOut.size(), equalTo( 0 ) );
     }
-
 }
--- util/src/test/java/org/apache/directory/api/util/exception/RuntimeMultiExceptionTest.java
+++ util/src/test/java/org/apache/directory/api/util/exception/RuntimeMultiExceptionTest.java
@@ -1,3 +1,22 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *  
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License. 
+ *  
+ */
 package org.apache.directory.api.util.exception;
 
 
@@ -15,9 +34,12 @@ import org.junit.Before;
 import org.junit.Test;
 
 
+/**
+ * 
+ * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
+ */
 public class RuntimeMultiExceptionTest
 {
-
     private PrintStream originalOut;
     private PrintStream originalErr;
 
@@ -80,5 +102,4 @@ public class RuntimeMultiExceptionTest
         assertThat( customOut.toString(), containsString( "nested2" ) );
         assertThat( systemOut.size(), equalTo( 0 ) );
     }
-
 }
