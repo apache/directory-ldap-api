b84821ad7142d21a0f9d9ac0fa7f498e949ff84d
Emmanuel Lécharny <elecharny@apache.org>
2017-08-17 16:50:03 +0000

    Removed the concurrency part : it sometime fails some tests.
    git-svn-id: https://svn.apache.org/repos/asf/directory/shared/branches/shared-value@1805317 13f79535-47bb-0310-9956-ffa450edef68

--- integ/src/test/java/org/apache/directory/api/ldap/entry/SchemaAwareEntrySerializationTest.java
+++ integ/src/test/java/org/apache/directory/api/ldap/entry/SchemaAwareEntrySerializationTest.java
@@ -36,10 +36,6 @@ import org.apache.directory.api.ldap.model.schema.SchemaManager;
 import org.apache.directory.api.ldap.schema.manager.impl.DefaultSchemaManager;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import com.mycila.junit.concurrent.Concurrency;
-import com.mycila.junit.concurrent.ConcurrentJunitRunner;
 
 
 /**
@@ -47,8 +43,6 @@ import com.mycila.junit.concurrent.ConcurrentJunitRunner;
  * 
  * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
  */
-@RunWith(ConcurrentJunitRunner.class)
-@Concurrency()
 public class SchemaAwareEntrySerializationTest
 {
     private static SchemaManager schemaManager;
