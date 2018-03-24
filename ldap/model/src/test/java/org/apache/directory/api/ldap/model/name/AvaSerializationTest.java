eb35e4e419b59706cd2f9eae7cf941b6a43b82be
Emmanuel Lécharny <elecharny@symas.com>
2018-03-24 21:04:27 +0100

    Fixed some more tests

--- ldap/model/src/test/java/org/apache/directory/api/ldap/model/name/AvaSerializationTest.java
+++ ldap/model/src/test/java/org/apache/directory/api/ldap/model/name/AvaSerializationTest.java
@@ -143,7 +143,7 @@ public class AvaSerializationTest
         catch ( IOException ioe )
         {
             String message = ioe.getMessage();
-            assertEquals( "Cannot serialize a wrong ATAV, the value should not be null", message );
+            assertEquals( "ERR_13619_CANNOT_SERIALIZE_AVA_VALUE_NULL Cannot serialize a wrong ATAV, the value should not be null", message );
         }
     }
 
@@ -164,7 +164,7 @@ public class AvaSerializationTest
         catch ( IOException ioe )
         {
             String message = ioe.getMessage();
-            assertEquals( "Cannot serialize a wrong ATAV, the value should not be null", message );
+            assertEquals( "ERR_13619_CANNOT_SERIALIZE_AVA_VALUE_NULL Cannot serialize a wrong ATAV, the value should not be null", message );
         }
     }
 
@@ -301,7 +301,7 @@ public class AvaSerializationTest
         catch ( IOException ioe )
         {
             String message = ioe.getMessage();
-            assertEquals( "Cannot serialize a wrong ATAV, the value should not be null", message );
+            assertEquals( "ERR_13619_CANNOT_SERIALIZE_AVA_VALUE_NULL Cannot serialize a wrong ATAV, the value should not be null", message );
         }
     }
 
