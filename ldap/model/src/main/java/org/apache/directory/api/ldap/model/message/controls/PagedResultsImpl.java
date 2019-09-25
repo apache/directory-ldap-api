7a6cd62ec4ea807225134e0ce838cd8dd2b367e9
emmanuel lecharny <emmanuel.lecharny@busit.com>
2019-09-25 04:36:18 +0200

    Fixed a checkstyle error

--- ldap/model/src/main/java/org/apache/directory/api/ldap/model/message/controls/PagedResultsImpl.java
+++ ldap/model/src/main/java/org/apache/directory/api/ldap/model/message/controls/PagedResultsImpl.java
@@ -20,7 +20,6 @@
 package org.apache.directory.api.ldap.model.message.controls;
 
 
-import org.apache.directory.api.util.Base64;
 import org.apache.directory.api.util.Strings;
 
 import java.util.Arrays;
