f9b04c40f8b975ce71b1eec20ad32bdee3f394ce
emmanuel lecharny <elecharny@apache.org>
2025-02-13 05:21:37 +0100

    Rollbacking the 2.1.8 release

--- ldap/client/api/src/main/java/org/apache/directory/ldap/client/api/LdapNetworkConnection.java
+++ ldap/client/api/src/main/java/org/apache/directory/ldap/client/api/LdapNetworkConnection.java
@@ -5030,6 +5030,8 @@ public class LdapNetworkConnection extends AbstractLdapConnection implements Lda
             {
                 sslFilter.setEnabledCipherSuites( enabledCipherSuite );
             }
+            
+            //sslFilter.setUseNonBlockingPipeline( true );
 
             // Be sure we disable SSLV3
             String[] enabledProtocols = config.getEnabledProtocols();
